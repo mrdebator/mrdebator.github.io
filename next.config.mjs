9412ee799d6ad6860bf9ba899c0c8801efe024a3
Ansh Chandnani <ansh.chandnani@gmail.com>
2025-08-17 17:10:59 -0700

    Remove basePath for user page deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,6 @@
 const nextConfig = {
   // Configure Next.js to export a static site
   output: 'export',
-
-  basePath: '/mrdebator.github.io',
-  assetPrefix: '/mrdebator.github.io/',
 };
 
 export default nextConfig;
