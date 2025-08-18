9b95cabc6e14534d4cc46835740556099fe02c35
Ansh Chandnani <ansh.chandnani@gmail.com>
2025-08-17 17:00:36 -0700

    Fix basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   // Configure Next.js to export a static site
   output: 'export',
+
+  basePath: 'mrdebator.github.io',
+  assetPrefix: 'mrdebator.github.io',
 };
 
 export default nextConfig;
